dce7956e3df4d043d3058759dc24b025e00325a1
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-23 00:23:28 -0300

    Criando ProdutoDAO

---
+++ src/main/java/dao/ProdutoDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import model.Produto;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class ProdutoDAO {
+    private EntityManager em;
+
+    public ProdutoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Produto produto) {
+        this.em.persist(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return this.em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return this.em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return this.em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+}
