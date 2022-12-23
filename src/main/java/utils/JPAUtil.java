bc3927a737fdaab707924f2f5e72953ea3ba18bd
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-23 00:50:16 -0300

    Edit JPAUtil

--- src/main/java/utils/JPAUtil.java
+++ src/main/java/utils/JPAUtil.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 public class JPAUtil {
     private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("loja");
 
-    private static EntityManager getEntityManager() {
+    public static EntityManager getEntityManager() {
         return FACTORY.createEntityManager();
     }
 }
