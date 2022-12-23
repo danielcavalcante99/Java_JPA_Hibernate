94952bee37a3abe8558dfdcdbcae70474056fd0c
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-23 00:00:37 -0300

    Configurando EntityManager

---
+++ src/main/java/utils/JPAUtil.java
@@ -0,0 +1,13 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("loja");
+
+    private static EntityManager getEntityManager() {
+        return FACTORY.createEntityManager();
+    }
+}
