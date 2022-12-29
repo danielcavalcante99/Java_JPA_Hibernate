b9b9d45bc88e49380d1c3840fe8488c455a3bf28
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-29 00:02:34 -0300

    Program

--- src/main/java/application/ProdutoApplication.java
+++ src/main/java/application/ProdutoApplication.java
@@ -1,67 +1,69 @@
-package resource;
+package application;
 
+import dao.ProdutoDAO;
+import model.Categoria;
 import model.Produto;
-import services.ProdutoService;
+import utils.JPAUtil;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
-@Path("/produto")
-public class ProdutoResource {
+public class ProdutoApplication {
 
-    private static ProdutoService service = new ProdutoService();
+    public static void main(String[] args) {
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO dao = new ProdutoDAO(em);
 
-    @GET
-    @Path("/buscartodos")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response buscarTodos() {
-        List<Produto> listProduto = ProdutoResource.service.buscarTodos();
-        return Response.ok(listProduto).build();
+        Produto celular = new Produto();
+        celular.setNome("Samsung S10");
+        celular.setPreco(new BigDecimal(2500.89));
+        celular.setCategoria(Categoria.CELULARES);
+        celular.setDescricao("Samsung Galaxy S10 128 GB preto-prisma 8 GB RAM");
+
+        Produto livro = new Produto();
+        livro.setNome("O poder do hábito");
+        livro.setPreco(new BigDecimal(36.99));
+        livro.setCategoria(Categoria.LIVROS);
+        livro.setDescricao("O poder do hábito Capa comum ");
+
+        List<Produto> listProduto = List.of(celular, livro);
+
+        //insert
+        System.out.println("INSERT: ");
+        em.getTransaction().begin();
+        listProduto.forEach(p -> dao.cadastrar(p));
+        em.clear();
+        System.out.println("...");
+
+        //update
+        System.out.println("UPDATE: ");
+        celular.setPreco(new BigDecimal(2800.99));
+        System.out.println(celular);
+        dao.atualizar(celular);
+        em.flush();
+        em.clear();
+        System.out.println("...");
+
+        //select  buscar todos
+        System.out.println("SELECT: Buscar Todos");
+        dao.buscarTodos().forEach(System.out::println);
+        System.out.println("...");
+
+        //select  buscar por ID
+        System.out.println("SELECT: Buscar por ID");
+        System.out.println(dao.buscarPorId(2L));
+        System.out.println("...");
+
+        //delete
+        System.out.println("DELETE: ");
+        System.out.println(dao.deletar(celular));
+        System.out.println("...");
+
+        //select  buscar todos
+        System.out.println("SELECT: Buscar Todos");
+        dao.buscarTodos().forEach(System.out::println);
+        System.out.println("...");
+
     }
-
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response buscarPorId(@PathParam("id") Long id) {
-        Produto produto = ProdutoResource.service.buscarPorId(id);
-        return Response.ok(produto).build();
-    }
-
-    @GET
-    @Path("/buscarpornome/{nome}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response buscarPorNome(@PathParam("nome") String nome) {
-        List<Produto> produtos = ProdutoResource.service.buscarPorNome(nome);
-        return Response.ok(produtos).build();
-    }
-
-    @POST
-    @Path("/cadastrar")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response cadastrar(Produto novoProduto) {
-        ProdutoResource.service.cadastrar(novoProduto);
-        return Response.status(Response.Status.CREATED).entity(novoProduto).build();
-    }
-
-    @PUT
-    @Path("/atualizar")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response atualizar(Produto atualizarProduto) {
-        ProdutoResource.service.atualizar(atualizarProduto);
-        return Response.status(Response.Status.CREATED).entity(atualizarProduto).build();
-    }
-
-    @DELETE
-    @Path("/deletar")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deletar(Produto deletarProduto) {
-        ProdutoResource.service.deletar(deletarProduto);
-        return Response.status(Response.Status.CREATED).entity(deletarProduto).build();
-    }
-
 }
