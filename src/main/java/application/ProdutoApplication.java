a14c2aef774bef9c4c7880e7d426f017c52cc4b0
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-29 00:49:23 -0300

    Edit Program Main

--- src/main/java/application/ProdutoApplication.java
+++ src/main/java/application/ProdutoApplication.java
@@ -11,59 +11,70 @@ import java.util.List;
 
 public class ProdutoApplication {
 
+    private static EntityManager em = JPAUtil.getEntityManager();
+    private static ProdutoDAO dao = new ProdutoDAO(em);
+
     public static void main(String[] args) {
-        EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO dao = new ProdutoDAO(em);
 
-        Produto celular = new Produto();
-        celular.setNome("Samsung S10");
-        celular.setPreco(new BigDecimal(2500.89));
-        celular.setCategoria(Categoria.CELULARES);
-        celular.setDescricao("Samsung Galaxy S10 128 GB preto-prisma 8 GB RAM");
+        try {
+            Produto celular = new Produto();
+            celular.setNome("Samsung S10");
+            celular.setPreco(new BigDecimal(2500.89));
+            celular.setCategoria(Categoria.CELULARES);
+            celular.setDescricao("Samsung Galaxy S10 128 GB preto-prisma 8 GB RAM");
 
-        Produto livro = new Produto();
-        livro.setNome("O poder do hábito");
-        livro.setPreco(new BigDecimal(36.99));
-        livro.setCategoria(Categoria.LIVROS);
-        livro.setDescricao("O poder do hábito Capa comum ");
+            Produto livro = new Produto();
+            livro.setNome("O poder do hábito");
+            livro.setPreco(new BigDecimal(36.99));
+            livro.setCategoria(Categoria.LIVROS);
+            livro.setDescricao("O poder do hábito Capa comum ");
 
-        List<Produto> listProduto = List.of(celular, livro);
+            //insert
+            System.out.println("INSERT: ");
+            ProdutoApplication.em.getTransaction().begin();
+            List<Produto> listProduto = List.of(celular, livro);
+            listProduto.forEach(p -> ProdutoApplication.dao.cadastrar(p));
+            ProdutoApplication.em.clear();
+            System.out.println("...");
 
-        //insert
-        System.out.println("INSERT: ");
-        em.getTransaction().begin();
-        listProduto.forEach(p -> dao.cadastrar(p));
-        em.clear();
-        System.out.println("...");
+            //update
+            System.out.println("UPDATE: ");
+            celular.setPreco(new BigDecimal(2800.99));
+            System.out.println(celular);
+            ProdutoApplication.dao.atualizar(celular);
+            ProdutoApplication.em.flush();
+            ProdutoApplication.em.clear();
+            System.out.println("...");
 
-        //update
-        System.out.println("UPDATE: ");
-        celular.setPreco(new BigDecimal(2800.99));
-        System.out.println(celular);
-        dao.atualizar(celular);
-        em.flush();
-        em.clear();
-        System.out.println("...");
+            //select  buscar todos
+            System.out.println("SELECT: Buscar Todos");
+            ProdutoApplication.dao.buscarTodos().forEach(System.out::println);
+            System.out.println("...");
 
-        //select  buscar todos
-        System.out.println("SELECT: Buscar Todos");
-        dao.buscarTodos().forEach(System.out::println);
-        System.out.println("...");
+            //select  buscar por ID
+            System.out.println("SELECT: Buscar por ID");
+            System.out.println(ProdutoApplication.dao.buscarPorId(2L));
+            System.out.println("...");
 
-        //select  buscar por ID
-        System.out.println("SELECT: Buscar por ID");
-        System.out.println(dao.buscarPorId(2L));
-        System.out.println("...");
+            //delete
+            System.out.println("DELETE: ");
+            System.out.println(ProdutoApplication.dao.deletar(celular));
+            System.out.println("...");
 
-        //delete
-        System.out.println("DELETE: ");
-        System.out.println(dao.deletar(celular));
-        System.out.println("...");
+            //select  buscar todos
+            System.out.println("SELECT: Buscar Todos");
+            ProdutoApplication.dao.buscarTodos().forEach(System.out::println);
+            System.out.println("...");
 
-        //select  buscar todos
-        System.out.println("SELECT: Buscar Todos");
-        dao.buscarTodos().forEach(System.out::println);
-        System.out.println("...");
+            ProdutoApplication.em.getTransaction().commit();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (ProdutoApplication.em.getTransaction().isActive()) {
+                ProdutoApplication.em.getTransaction().rollback();
+            }
+        } finally {
+            ProdutoApplication.em.close();
+        }
     }
 }
